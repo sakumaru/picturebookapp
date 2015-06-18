20968ce423f98bd6a9750677241f8cdac012d09f
Ryo Sakurai <ptolemaios923@icloud.com>
2015-06-18 15:57:34 +0900

    first commit

---
+++ picturebook/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  picturebook
+//
+//  Created by 桜井諒 on 2015/06/18.
+//  Copyright (c) 2015年 桜井諒. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+let myTextField:UITextField =
+UITextField(frame:CGRectMake(0,0,500,30))
+myTextField.text = "Hallo World"
+myTextField.delegate = self;
+myTextField.borderStyle = UITextBorderStyle.RoundedRect
+self.view.addSubview(myTextField)
+
+//編集直後
+func textFieldDidBeginEditing(textField:UITextField){
+    println("textFieldDidBeginEditing:"+textField.text)
+}
+
+//編集完了後(編集直後)
+func textFieldShouldEndEditing(textField:UITextField) ->Bool{
+    println("textFieldShouldEndEditing:"+textField.text)
+    return true
+}
+
+//編集完了後(完了直後)
+func textFieldShouldReturn(textField:UITextField)->Bool{
+    textField.resignFirstResponder()
+    return true
+}
